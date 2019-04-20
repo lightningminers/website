437d625f4ebe47e4d9d94666b704e76a645b191c
icepy <xiangwenwe@foxmail.com>
2019-04-20 19:22:45 +0800

    [add] 添加闪电矿工翻译组网站

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import icon from "./img/favicon.png";
+import articles from "./articles";
+import bg from "./img/5c07e6dcbd9df.png";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="container">
+          <header className="header">
+            <img src={icon} />
+            <span className="header-text">闪电矿工翻译组</span>
+          </header>
+          <div className="content">
+            <header className="content-header">
+              <img src={bg} />
+            </header>
+            <div className="content-container">
+              {
+                articles.map((v) => {
+                  return (
+                    <div key={v.id} className="item">
+                      <a
+                        href={v.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {v.date}: {v.name}
+                      </a>
+                    </div>
+                  )
+                })
+              }
+            </div>
+            <footer className="copyright">
+              <h2>Copyright</h2>
+              <p>
+                版权声明: <a target="_blank" rel="noopener noreferrer" href="https://github.com/lightningminers/article">闪电矿工翻译组</a> 译文仅用于学习、研究和交流。
+              </p>
+              <p>
+                版权归: <a target="_blank" rel="noopener noreferrer" href="https://github.com/lightningminers/article">闪电矿工翻译组</a>、文章作者和译者所有，欢迎非商业转载。
+              </p>
+              <p>
+                转载前请联系译者或 <a target="_blank" rel="noopener noreferrer" href="https://github.com/icepy">管理员 @icepy</a> 获取授权，并在文章开头明显位置注明本文出处、译者、校对者和闪电矿工翻译组的完整链接，违者必究。
+              </p>
+            </footer>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/articles.ts
@@ -0,0 +1,27 @@
+import createUUID from "usedjs/lib/createUUID";
+
+export interface IArticle {
+  id: string;
+  name: string;
+  url: string;
+  date: string;
+}
+
+export type Articles = IArticle;
+
+const articles = [
+  {
+    id: createUUID(),
+    name: "理解 JavaScript 中的循环",
+    url: "https://zhuanlan.zhihu.com/p/62246809",
+    date: "2019-04-12"
+  },
+  {
+    id: createUUID(),
+    name: "ES5 to ESNext —  自 2015 以来 JavaScript 新增的所有新特性",
+    url: "https://zhuanlan.zhihu.com/p/59535309",
+    date: "2019-03-17"
+  }
+];
+
+export default articles;
